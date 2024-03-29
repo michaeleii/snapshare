5dd17b925fa237b353a1908df8c7c42f4cdde851
Michael Lei <michaelleie0ca@gmail.com>
2024-03-28 17:26:30 -0700

    Convert to monorepo

--- sst.config.ts
+++ sst.config.ts
@@ -6,6 +6,11 @@ export default $config({
       name: "snapshare",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
+      providers: {
+        aws: {
+          region: "us-west-2",
+        },
+      },
     };
   },
   async run() {},

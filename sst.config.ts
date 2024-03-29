1d28db2696b30fe0ae9eee6b7dbf48fe4bbd9243
Michael Lei <michaelleie0ca@gmail.com>
2024-03-28 18:17:55 -0700

    Add an api gateway

--- sst.config.ts
+++ sst.config.ts
@@ -13,5 +13,15 @@ export default $config({
       },
     };
   },
-  async run() {},
+  async run() {
+    const api = new sst.aws.ApiGatewayV2("snapshare-api");
+
+    // Posts
+    const postHandler = "packages/functions/posts.handler";
+    api.route("GET /posts/ui", postHandler);
+    api.route("GET /posts", postHandler);
+    api.route("GET /posts/:id", postHandler);
+    api.route("POST /posts", postHandler);
+    api.route("DELETE /posts/:id", postHandler);
+  },
 });

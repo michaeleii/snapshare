ac66dbb8820743a9337ab1b8eaf4e3e5d53173d4
Michael Lei <michaelleie0ca@gmail.com>
2024-03-28 19:39:33 -0700

    Finish db table setup

--- packages/core/src/db/schema.ts
+++ packages/core/src/db/schema.ts
@@ -6,14 +6,27 @@ import {
   uniqueIndex,
 } from "drizzle-orm/sqlite-core";
 
+export const users = sqliteTable("users", {
+  id: text("id").notNull().primaryKey(),
+  title: text("title", { length: 255 }).notNull(),
+  username: text("username").notNull(),
+  createdAt: text("created_at")
+    .default(sql`(CURRENT_TIMESTAMP)`)
+    .notNull(),
+});
+
 export const posts = sqliteTable(
   "posts",
   {
-    id: integer("id").primaryKey(),
-    userId: integer("userId"),
-    title: text("title", { length: 255 }),
-    content: text("content", { length: 1000 }),
-    createdAt: text("created_at").default(sql`(CURRENT_TIMESTAMP)`),
+    id: integer("id").notNull().primaryKey(),
+    userId: text("user_id")
+      .notNull()
+      .references(() => users.id),
+    title: text("title", { length: 255 }).notNull(),
+    content: text("content", { length: 1000 }).notNull(),
+    createdAt: text("created_at")
+      .default(sql`(CURRENT_TIMESTAMP)`)
+      .notNull(),
   },
   (posts) => ({
     userIdx: uniqueIndex("userIdx").on(posts.userId),

aef91099738403273094fa4be7463d7b66a79f1f
Michael Lei <michaelleie0ca@gmail.com>
2024-03-29 23:14:18 -0700

    Add profile page

--- packages/web/src/routes/_authenticated/profile.lazy.tsx
+++ packages/web/src/routes/_authenticated/profile.lazy.tsx
@@ -1,5 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
 import { createLazyFileRoute } from "@tanstack/react-router";
 
 export const Route = createLazyFileRoute("/_authenticated/profile")({
-  component: () => <div>Hello /profile!</div>,
+  component: Profile,
 });
+
+export default function Profile() {
+  const { user, logout } = useKindeAuth();
+  return (
+    <div className="mx-auto max-w-[468px] px-2 pt-10">
+      <h1 className="mb-4 text-2xl font-bold">Hi {user?.given_name}</h1>
+      <p className="mb-4 text-gray-500">{user?.email}</p>
+      <Button onClick={() => logout()}>Logout</Button>
+    </div>
+  );
+}

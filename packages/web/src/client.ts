570794588d895c91ad16c57cb587dd1b7df726ed
Michael Lei <michaelleie0ca@gmail.com>
2024-03-30 14:44:47 -0700

    hono rpc doesn't work reverting back to fetch with type casting

--- packages/web/src/client.ts
+++
@@ -1,6 +0,0 @@
-import { type PostApiType } from "@functions/posts";
-import { hc } from "hono/client";
-
-export const postApi = hc<PostApiType>(
-  "https://a5uzo4f60j.execute-api.us-west-2.amazonaws.com/",
-);
